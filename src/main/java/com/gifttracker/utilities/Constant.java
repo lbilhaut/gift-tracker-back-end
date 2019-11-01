e4fa2baebe2c2503ad2781599cb11046b2a58f60
WhiteWhate <lise.bilhaut@gmail.com>
2019-11-01 12:06:00 -0400

    change upload path

--- src/main/java/com/gifttracker/utilities/Constant.java
+++ src/main/java/com/gifttracker/utilities/Constant.java
@@ -4,9 +4,12 @@ public class Constant {
 //	static String RELATIVE_PATH = "/KidsGiftTracker2";  // For local development
 	public static String RELATIVE_PATH = "";  // For deployment
 
-	
-	public static String UPLOAD_PATH = "C:\\Users\\Lise Bilhaut\\eclipse-workspace\\KidsGiftTracker2\\src\\main\\webapp\\img\\";
-//	public static String UPLOAD_PATH = "webapp\\img\\";
+// For local development
+//	public static String UPLOAD_PATH = "C:\\Users\\Lise Bilhaut\\eclipse-workspace\\KidsGiftTracker2\\src\\main\\webapp\\img\\";
+
+// For deployment
+	public static String UPLOAD_PATH = "https://kids-gift-tracker.herokuapp.com/img/";
+
 	
 //	//Save the uploaded file to this folder
 //   public static String UPLOADED_FOLDER = "C:\\Users\\Lise Bilhaut\\eclipse-workspace\\KidsGiftTracker2\\src\\main\\webapp\\img\\upload\\";
